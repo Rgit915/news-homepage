a5ee1ea2d3b79b13866bd60b33e5649449fa7998
Rgit915 <rora.goitom@gmail.com>
2024-09-17 17:19:27 -0400

    Add newsLink and newList data arrays

---
+++ data.js
@@ -0,0 +1,36 @@
+export const newsLink = [
+  {
+    image: "./assets/images/image-retro-pcs.jpg",
+    title: "Reviving Retro PCs",
+    description: "What happens when old PCs are given modern upgrades?",
+    number: "01",
+  },
+  {
+    image: "./assets/images/image-top-laptops.jpg",
+    title: "Top 10 Laptops of 2022 ",
+    description: "Our best picks for various needs and budgets.",
+    number: "02",
+  },
+  {
+    image: "./assets/images/image-gaming-growth.jpg",
+    title: "The Growth of Gaming ",
+    description: "How the pandemic has sparked fresh opportunities.",
+    number: "03",
+  },
+
+];
+
+export const newList = [
+  {
+    title: "Hydrogen VS Electric Cars",
+    description: "Will hydrogen-fueled cars ever catch up to EVs?",
+  },
+  {
+    title: "The Downsides of AI Artistry",
+    description: "What are the possible adverse effects of on-demand AI image generation?",
+  },
+  {
+    title: "Is VC Funding Drying Up?",
+    description: "How much funding is being sought after the pandemic?",
+  },
+];
