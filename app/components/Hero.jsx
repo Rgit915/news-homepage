5e9fd1c9928aacd36d0640bab3a6792b94fe9f4a
Rgit915 <rora.goitom@gmail.com>
2024-09-18 15:55:38 -0400

    Add mobile-first styles

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,34 +1,32 @@
-import Image from "next/image"
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="hero-section">
-          <div className="hero-image">
-            <Image
-              src="/assets/images/image-web-3-mobile.jpg"
-              width={343}
-              height={300}
-              alt="hero image"
-              layout="responsive"
-              sizes="(max-width: 600px) 100vw, (max-width:1200px) 50vw, 33vw"
-              priority
-            />
-          </div>
-          <div className="hero-heading">
-            <div>
-              <h1>The Bright Future of Web 3.0?</h1>
-            </div>
-          </div>
-          <div className="hero-messsage">
-            <p>
-              We dive into the next evolution of the web that claims to put the
-              power of the platforms back into the hands of the people. But is
-              it really fulfilling its promise?
-            </p>
-            <button>READ MORE</button>
-          </div>
-        </section>
-  )
-}
+    <section className="hero-section flex flex-col p-4 gap-4">
+      <div className="hero-image">
+        <Image
+          src="/assets/images/image-web-3-mobile.jpg"
+          width={343}
+          height={300}
+          alt="hero image"
+          layout="responsive"
+          sizes="(max-width: 600px) 100vw, (max-width:1200px) 50vw, 33vw"
+          priority
+        />
+      </div>
+      <div className="hero-heading text-very-dark-blue text-4xl font-extrabold pr-12">
+        <h1>The Bright Future of Web 3.0?</h1>
+      </div>
+      <div className="hero-messsage space-y-4">
+        <p className="font-normal text-base leading-6 text-gunmetal">
+          We dive into the next evolution of the web that claims to put the
+          power of the platforms back into the hands of the people. But is it
+          really fulfilling its promise?
+        </p>
+        <button className="bg-soft-red px-6 py-2 text-very-dark-blue text-sm font-bold leading-6 tracking-widest">READ MORE</button>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
--- app/components/New.jsx
+++ app/components/New.jsx
@@ -2,15 +2,15 @@ import { newList } from "../../data";
 
 const New = () => {
   return (
-    <section className="new-section">
-      <article className="">
-        <h2>New</h2>
+    <section className="new-section p-4">
+      <article className="p-6 flex flex-col gap-6 bg-very-dark-blue text-off-white">
+        <h2 className="text-soft-orange font-bold text-4xl  ">New</h2>
         {newList.map((item, index) => (
-          <div key={index}>
-            <h3>
+          <div key={index} className="">
+            <h3 className="text-xl font-extrabold">
               {item.title}
-              <span>{item.description}</span>
             </h3>
+              <span className="text-grayish-blue text-sm font-normal">{item.description}</span>
           </div>
         ))}
       </article>
