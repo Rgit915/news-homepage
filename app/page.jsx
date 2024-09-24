f9b9212cc1394dbeb5e32a4d1ede37f1203d70a2
Rgit915 <rora.goitom@gmail.com>
2024-09-24 10:11:47 -0400

    Fix typo class to className

--- app/page.jsx
+++ app/page.jsx
@@ -15,9 +15,9 @@ export default function Home() {
         <NewsLink />
       </main>
       <footer>
-      <div class="attribution text-[0.73rem] text-center opacity-80 text-[#8c8c8c] mt-12">
+      <div className="attribution text-[0.73rem] text-center opacity-80 text-[#8c8c8c] mt-12">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-    Coded by <a href="https://www.linkedin.com/in/rora-alem" target="_blank" class="text-[hsl(228,45%,44%)]">Rora Alem</a>.
+    Coded by <a href="https://www.linkedin.com/in/rora-alem" target="_blank" className="text-[hsl(228,45%,44%)]">Rora Alem</a>.
   </div>
       </footer>
     </>
